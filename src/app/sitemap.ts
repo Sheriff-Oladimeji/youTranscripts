350a08e6dabf1b710153166b505762827a78eba2
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2025-04-30 13:14:50 +0100

    fix: sitempa error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,41 +1,39 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://youtranscripts.com';
-  
+  const baseUrl = "https://youtranscripts.com";
+
   // Define the pages that should be included in the sitemap
-  const routes = [
+  return [
     {
       url: `${baseUrl}`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly" as const,
       priority: 1.0,
     },
     {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly" as const,
       priority: 0.8,
     },
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly" as const,
       priority: 0.8,
     },
     {
       url: `${baseUrl}/terms`,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly" as const,
       priority: 0.5,
     },
     {
       url: `${baseUrl}/privacy`,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly" as const,
       priority: 0.5,
     },
   ];
-
-  return routes;
 }
